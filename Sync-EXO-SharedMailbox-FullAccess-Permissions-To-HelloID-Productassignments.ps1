9ac6267b1c6072d8731edbf0f1311fbaab986297
mspreeuwenberg <76214805+mspreeuwenberg@users.noreply.github.com>
2026-05-04 13:57:50 +0200

    Feat certificate support (#5)
    * feature: added certificate support
    
    * Updated readme
    
    * Update README
    
    ---------
    
    Co-authored-by: rhouthuijzen <116062840+rhouthuijzen@users.noreply.github.com>

--- Sync-EXO-SharedMailbox-FullAccess-Permissions-To-HelloID-Productassignments.ps1
+++ Sync-EXO-SharedMailbox-FullAccess-Permissions-To-HelloID-Productassignments.ps1
@@ -210,6 +210,19 @@ function Invoke-HIDRestmethod {
         throw $_
     }
 }
+
+function Get-MSEntraCertificate {
+    [CmdletBinding()]
+    param()
+    try {
+        $rawCertificate = [system.convert]::FromBase64String($EntraIdCertificateBase64String)
+        $certificate = [System.Security.Cryptography.X509Certificates.X509Certificate2]::new($rawCertificate, $EntraIdCertificatePassword, [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::Exportable)
+        Write-Output $certificate
+    }
+    catch {
+        $PSCmdlet.ThrowTerminatingError($_)
+    }
+}
 #endregion functions
 
 #region script
@@ -371,61 +384,51 @@ catch {
 
 Hid-Write-Status -Event Information -Message "------[Exchange Online]-----------"
 
-# Import module
-try {
-    $moduleName = "ExchangeOnlineManagement"
-    $importModule = Import-Module -Name $moduleName -ErrorAction Stop -Verbose:$false
-}
+try {    
+    $actionMessage = "importing module [ExchangeOnlineManagement]"
+    $importModuleSplatParams = @{
+        Name        = "ExchangeOnlineManagement"
+        Cmdlet      = $exchangeOnlineCommands
+        Verbose     = $false
+        ErrorAction = "Stop"
+    }
+    $null = Import-Module @importModuleSplatParams
+
+    #region Retrieving certificate
+    $actionMessage = "retrieving certificate"
+    $certificate = Get-MSEntraCertificate
+    #endregion Retrieving certificate
+    
+    #region Connect to Microsoft Exchange Online
+    # Docs: https://learn.microsoft.com/en-us/powershell/module/exchange/connect-exchangeonline?view=exchange-ps
+    $actionMessage = "connecting to Microsoft Exchange Online"
+    $createExchangeSessionSplatParams = @{
+        Organization          = $EntraIdOrganization
+        AppID                 = $EntraIdAppId
+        Certificate           = $certificate
+        CommandName           = $commands
+        ShowBanner            = $false
+        ShowProgress          = $false
+        TrackPerformance      = $false
+        SkipLoadingCmdletHelp = $true
+        SkipLoadingFormatData = $true
+        ErrorAction           = "Stop"
+    }
+    $null = Connect-ExchangeOnline @createExchangeSessionSplatParams
+    Write-Information "Connected to Microsoft Exchange Online"
+} 
 catch {
     $ex = $PSItem
-    $errorMessage = Get-ErrorMessage -ErrorObject $ex
-
-    Write-Verbose "Error at Line [$($ex.InvocationInfo.ScriptLineNumber)]: $($ex.InvocationInfo.Line). Error: $($($errorMessage.VerboseErrorMessage))"
-
-    throw "Error importing module [$moduleName]. Error Message: $($errorMessage.AuditErrorMessage)"
-}
-
-# Connect to Exchange
-try {
-    # Create access token
-    Write-Verbose "Creating Access Token"
-
-    $baseUri = "https://login.microsoftonline.com/"
-    $authUri = $baseUri + "$EntraTenantID/oauth2/token"
-    
-    $body = @{
-        grant_type    = "client_credentials"
-        client_id     = "$EntraAppID"
-        client_secret = "$EntraAppSecret"
-        resource      = "https://outlook.office365.com"
+    if (-not [string]::IsNullOrEmpty($ex.Exception.Data.RemoteException.Message)) {
+        $warningMessage = "Error at Line [$($ex.InvocationInfo.ScriptLineNumber)]: $($ex.InvocationInfo.Line). Error: $($ex.Exception.Data.RemoteException.Message)"
+        $auditMessage = "Error $($actionMessage). Error: $($ex.Exception.Data.RemoteException.Message)"        
     }
-    
-    $Response = Invoke-RestMethod -Method POST -Uri $authUri -Body $body -ContentType "application/x-www-form-urlencoded" -UseBasicParsing:$true -Verbose:$false
-    $accessToken = $Response.access_token
-
-    # Connect to Exchange Online in an unattended scripting scenario using an access token.
-    Write-Verbose "Connecting to Exchange Online"
-
-    $exchangeSessionParams = @{
-        Organization     = $EntraOrganization
-        AppID            = $EntraAppID
-        AccessToken      = $accessToken
-        CommandName      = $exchangeOnlineCommands
-        ShowBanner       = $false
-        ShowProgress     = $false
-        TrackPerformance = $false
-        ErrorAction      = "Stop"
+    else {
+        $warningMessage = "Error at Line [$($ex.InvocationInfo.ScriptLineNumber)]: $($ex.InvocationInfo.Line). Error: $($ex.Exception.Message)"
+        $auditMessage = "Error $($actionMessage). Error: $($ex.Exception.Message)"
     }
-    $exchangeSession = Connect-ExchangeOnline @exchangeSessionParams -Verbose:$false
-    
-    Write-Information "Successfully connected to Exchange Online"
-}
-catch {
-    $ex = $PSItem
-    $errorMessage = Get-ErrorMessage -ErrorObject $ex
-
-    Write-Verbose "Error at Line [$($ex.InvocationInfo.ScriptLineNumber)]: $($ex.InvocationInfo.Line). Error: $($errorMessage.VerboseErrorMessage)"
-    throw "Error connecting to Exchange Online. Error Message: $($errorMessage.AuditErrorMessage)"
+    Write-Warning $warningMessage
+    Write-Error $auditMessage
 }
 
 # Get Exchange Online Shared Mailboxes
